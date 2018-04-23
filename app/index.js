0067971e373b2d5717ae14c1ac97d9408342116e
Moe <marvel451@aol.com>
2018-04-22 23:08:07 -0700

    Added CSS
    Actual Glove/Hand Added. Also added Break function. Still need to integrate functionality to light up stones within minute increments for break time.

--- app/index.js
+++ app/index.js
@@ -9,6 +9,7 @@ class Timer extends React.Component {
 	  this.startTimer = this.startTimer.bind(this);
 	  this.stopTimer = this.stopTimer.bind(this);
 	  this.resetTimer = this.resetTimer.bind(this);
+	  this.breakTimer = this.breakTimer.bind(this);
 	  this.countDown = this.countDown.bind(this);
 	}
 
@@ -20,8 +21,13 @@ class Timer extends React.Component {
 
 	  let divisor_for_seconds = divisor_for_minutes % 60;
 	  let seconds = Math.ceil(divisor_for_seconds);
-	  if (seconds < 10) seconds = '0' + Math.ceil(divisor_for_seconds);
 
+	  // add a zero if seconds are less than 10
+	  if (seconds < 10) seconds = '0' + Math.ceil(divisor_for_seconds);
+	  // Check if we're at zero.
+	  if (seconds == 0 && minutes == 0) { 
+	    clearInterval(this.timer);
+	  }
 
 	  let obj = {
 	    "h": hours,
@@ -52,6 +58,12 @@ class Timer extends React.Component {
 	  this.setState({  seconds: 1500+1 });
 	 }
 
+	 breakTimer() {
+	   clearInterval(this.timer);
+	   this.timer =  setInterval(this.countDown, 1000);
+	   this.setState({  seconds: 60*5+1 });
+	  }
+
 	countDown() {
 	  // Remove one second, set state so a re-render happens.
 	  let seconds = this.state.seconds - 1;
@@ -72,11 +84,11 @@ class Timer extends React.Component {
 	  	document.querySelector(".mind").style.opacity = '1';
 	  }
 
-	  if(this.state.timeElapsed ==  24.9 * 60) {
+	  if(this.state.timeElapsed ==  15 * 60) {
 	  	document.querySelector(".reality").style.opacity = '1';
 	  }
 
-	  if(this.state.timeElapsed ==  24.5 * 60) {
+	  if(this.state.timeElapsed ==  20 * 60) {
 	  	document.querySelector(".power").style.opacity = '1';
 	  }
 
@@ -86,10 +98,6 @@ class Timer extends React.Component {
 	  	alert("Break Time !");
 	  }
 	  
-	  // Check if we're at zero.
-	  if (seconds == 0) { 
-	    clearInterval(this.timer);
-	  }
 	}
 
 
@@ -105,20 +113,33 @@ class Timer extends React.Component {
 			   <button className="btn start" onClick={this.startTimer}>Start</button>
 		       <button className="btn pause" onClick={this.stopTimer}>Pause</button>
 		       <button className="btn reset" onClick={this.resetTimer}>Reset</button>
+		       <button className="btn break" onClick={this.breakTimer}>Break</button>
 
 				     
 				<p className="numbers">{this.state.time.m} : {this.state.time.s} </p >  
  			
 				
 				 <div className="gem-wrapper">
-				 	<div className="gem time"></div>
-				    <div className="gem mind"></div>
-				    <div className="gem reality"></div>
-				    <div className="gem power"></div>
-				    <div className="gem space"></div>
+				 	<div className="gem gem-oval time"></div>
+				    <div className="gem gem-square mind"></div>
+				    <div className="gem gem-square reality"></div>
+				    <div className="gem gem-oval power"></div>	  
 				</div>
 
-		   </div>
+
+				<div className="gauntlet">
+				    <div className="knuckle"></div>
+				    <div className="knuckle"></div>
+				    <div className="knuckle"></div>
+				    <div className="knuckle"></div>
+				    <div className="knuckle thumb"></div>
+				    <div className="space-wrapper">
+				        <div className="gem gem-oval space"></div>
+				    </div>
+				    <div className="arm"></div>
+				</div>
+
+			</div>
 
 		)
 	}
@@ -127,6 +148,6 @@ class Timer extends React.Component {
 
 ReactDOM.render(
 
-	<Timer />, 	document.getElementById('app')
+	<Timer />, 	document.getElementById('app');
 
 );
--- build/index.js
+++ build/index.js
@@ -9,6 +9,7 @@ class Timer extends React.Component {
 		this.startTimer = this.startTimer.bind(this);
 		this.stopTimer = this.stopTimer.bind(this);
 		this.resetTimer = this.resetTimer.bind(this);
+		this.breakTimer = this.breakTimer.bind(this);
 		this.countDown = this.countDown.bind(this);
 	}
 
@@ -20,7 +21,13 @@ class Timer extends React.Component {
 
 		let divisor_for_seconds = divisor_for_minutes % 60;
 		let seconds = Math.ceil(divisor_for_seconds);
+
+		// add a zero if seconds are less than 10
 		if (seconds < 10) seconds = '0' + Math.ceil(divisor_for_seconds);
+		// Check if we're at zero.
+		if (seconds == 0 && minutes == 0) {
+			clearInterval(this.timer);
+		}
 
 		let obj = {
 			"h": hours,
@@ -51,6 +58,12 @@ class Timer extends React.Component {
 		this.setState({ seconds: 1500 + 1 });
 	}
 
+	breakTimer() {
+		clearInterval(this.timer);
+		this.timer = setInterval(this.countDown, 1000);
+		this.setState({ seconds: 60 * 5 + 1 });
+	}
+
 	countDown() {
 		// Remove one second, set state so a re-render happens.
 		let seconds = this.state.seconds - 1;
@@ -71,11 +84,11 @@ class Timer extends React.Component {
 			document.querySelector(".mind").style.opacity = '1';
 		}
 
-		if (this.state.timeElapsed == 24.9 * 60) {
+		if (this.state.timeElapsed == 15 * 60) {
 			document.querySelector(".reality").style.opacity = '1';
 		}
 
-		if (this.state.timeElapsed == 24.5 * 60) {
+		if (this.state.timeElapsed == 20 * 60) {
 			document.querySelector(".power").style.opacity = '1';
 		}
 
@@ -83,11 +96,6 @@ class Timer extends React.Component {
 			document.querySelector(".space").style.opacity = '1';
 			clearInterval(this.timer);
 			alert("Break Time !");
-		}
-
-		// Check if we're at zero.
-		if (seconds == 0) {
-			clearInterval(this.timer);
 		}
 	}
 
@@ -117,6 +125,11 @@ class Timer extends React.Component {
 				"Reset"
 			),
 			React.createElement(
+				"button",
+				{ className: "btn break", onClick: this.breakTimer },
+				"Break"
+			),
+			React.createElement(
 				"p",
 				{ className: "numbers" },
 				this.state.time.m,
@@ -127,11 +140,25 @@ class Timer extends React.Component {
 			React.createElement(
 				"div",
 				{ className: "gem-wrapper" },
-				React.createElement("div", { className: "gem time" }),
-				React.createElement("div", { className: "gem mind" }),
-				React.createElement("div", { className: "gem reality" }),
-				React.createElement("div", { className: "gem power" }),
-				React.createElement("div", { className: "gem space" })
+				React.createElement("div", { className: "gem gem-oval time" }),
+				React.createElement("div", { className: "gem gem-square mind" }),
+				React.createElement("div", { className: "gem gem-square reality" }),
+				React.createElement("div", { className: "gem gem-oval power" })
+			),
+			React.createElement(
+				"div",
+				{ className: "gauntlet" },
+				React.createElement("div", { className: "knuckle" }),
+				React.createElement("div", { className: "knuckle" }),
+				React.createElement("div", { className: "knuckle" }),
+				React.createElement("div", { className: "knuckle" }),
+				React.createElement("div", { className: "knuckle thumb" }),
+				React.createElement(
+					"div",
+					{ className: "space-wrapper" },
+					React.createElement("div", { className: "gem gem-oval space" })
+				),
+				React.createElement("div", { className: "arm" })
 			)
 		);
 	}
